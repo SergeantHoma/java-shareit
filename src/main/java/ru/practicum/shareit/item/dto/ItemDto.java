1c94f39505c5f3e5be453159866a6b4d76fc0581
Valdimir Esin <vova.esin.00@mail.ru>
2024-11-10 04:58:23 +0300

    first version sprint

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,24 @@
 package ru.practicum.shareit.item.dto;
 
-/**
- * TODO Sprint add-controllers.
- */
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.request.ItemRequest;
+import ru.practicum.shareit.user.dto.UserDto;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ItemDto {
+    private  Long id;
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String description;
+    @NotNull
+    private Boolean available;
+    private UserDto owner;
+    private ItemRequest request;
 }
