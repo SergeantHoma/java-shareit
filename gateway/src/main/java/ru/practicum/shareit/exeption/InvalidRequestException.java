128304b388d07f03237ed40570eb614e5d122d75
Valdimir Esin <vova.esin.00@mail.ru>
2024-11-11 20:11:22 +0300

    test

--- gateway/src/main/java/ru/practicum/shareit/exeption/InvalidRequestException.java
+++ gateway/src/main/java/ru/practicum/shareit/exeption/InvalidRequestException.java
@@ -1,6 +1,6 @@
 package ru.practicum.shareit.exeption;
 
-public class InvalidRequestException extends RuntimeException{
+public class InvalidRequestException extends RuntimeException {
     public InvalidRequestException(String message) {
         super(message);
     }
